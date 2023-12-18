c4a413af51c8a88ada63dc788f6ab02dc18ed12a
Romain Ré <romain.re@gmail.com>
2023-12-18 00:52:00 -0500

    refactor : remove noStore in data fetch

--- src/logic/data.ts
+++ src/logic/data.ts
@@ -1,5 +1,4 @@
 import { sql } from '@vercel/postgres';
-import { unstable_noStore as noStore } from 'next/cache';
 import {
   Vault,
   Element,
@@ -7,67 +6,50 @@ import {
 } from './definitions';
 
 export async function fetchRootVaults() {
-  noStore();
-
   try {
     const vaults = await sql<Vault>`
       SELECT * FROM vaults
       WHERE vault_id IS NULL
     `;
-
     return vaults.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch root vaults.');
   }
-
 }
 export async function fetchVaultById(id: string) {
-  noStore();
-
   try {
     const vaults = await sql<Vault>`
       SELECT * FROM vaults
       WHERE id = ${id}
     `;
-
     return vaults.rows[0];
-
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch vault by id.');
   }
-
 }
 export async function fetchVaultsByVaultId(id: string) {
-  noStore();
-
   try {
     const vaults = await sql<Vault>`
       SELECT * FROM vaults
       WHERE vault_id = ${id}
     `;
-
     return vaults.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch vaults by vault id.');
   }
-
 }
 export async function fetchElementsByVaultId(id: string) {
-  noStore();
-
   try {
     const elements = await sql<Element>`
       SELECT * FROM elements
       WHERE vault_id = ${id}
     `;
-
     return elements.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch elements by vault id.');
   }
-
 }
