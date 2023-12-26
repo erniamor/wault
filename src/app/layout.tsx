ba785ff6a4bf31abe92bc494a636aaad2471f618
Romain Ré <romain.re@gmail.com>
2023-12-26 16:53:20 -0500

    feat : add pwa support and og data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,60 @@ import type { Metadata } from 'next'
 import { inter } from '@/assets/fonts';
 import '@/assets/globals.css';
 
+const APP_NAME = "Wault";
+const APP_DEFAULT_TITLE = "My Awesome Wault";
+const APP_TITLE_TEMPLATE = "%s | Wault";
+const APP_DESCRIPTION = "A vault for your notes, links and more!";
+const APP_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+
 export const metadata: Metadata = {
+  applicationName: APP_NAME,
   title: {
-    template: '%s | Wault',
-    default: 'Wault',
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
   },
-  description: 'A folder for your notes, links and more.',
-}
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    url: APP_URL,
+    images: [
+      {
+        url: APP_URL + '/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: APP_URL + '/og-alt.png',
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
 
 export default function RootLayout({
   children,
