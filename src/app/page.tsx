0484818854b9a9a18812ddfcd207faba8a230022
Romain Ré <romain.re@gmail.com>
2023-12-26 17:04:36 -0500

    refactor : update logo image size and wault font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import Button from '@/components/Button';
+import { lusitana } from '@/assets/fonts';
+import clsx from 'clsx';
 
 export default function Home() {
   return (
@@ -7,13 +9,13 @@ export default function Home() {
       <div className='flex flex-col gap-5'>
         <div className="flex flex-col items-center justify-center">
           <Image
-            src="/wault-logo.png"
+            src="/wault-logo-200.png"
             width={200}
             height={200}
             alt="Wault logo"
           />
-          <h1 className='text-6xl text-white mb-2'>Wault</h1>
-          <p className='text-white text-center'>A vault for your notes, links and more.</p>
+          <h1 className={clsx('text-6xl text-white mt-4 mb-2', lusitana.className)}>Wault</h1>
+          <p className='text-white text-center'>A vault for your notes, links and more!</p>
         </div>
         <div className='flex flex-row items-center justify-center gap-3 mt-20'>
           <Button href="/auth/login">Login</Button>
--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { inter } from '@/assets/fonts';
+import { lusitana } from '@/assets/fonts';
 
 export default async function HeaderLogo() {
   return (
-    <Link href={`/`} className="flex flex-row items-center gap-3">
+    <Link href={`/`} className="flex flex-row items-center justify-center gap-3">
       <Image
-        src="/wault-logo.png"
+        src="/wault-logo-40.png"
         width={40}
         height={40}
         alt="Wault logo"
       />
-      <h1 className={`${inter.className} text-2xl text-white`}>Wault</h1>
+      <h1 className={`${lusitana.className} text-3xl text-white `}>Wault</h1>
     </Link>
   );
 }
